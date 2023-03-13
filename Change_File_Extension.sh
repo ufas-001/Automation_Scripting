f992e58b2a929f175c15ab31db2d297287149b8c
ufas-001 <fasabdulmutolib@gmail.com>
2023-03-13 21:48:50 +0100

    added change_file_extension file

---
+++ Change_File_Extension.sh
@@ -0,0 +1,15 @@
+## Change file extension
+SRC="/mnt/c/Users/OWNER/Documents/Change_File_Extension"
+FROM_EXTENSION=".docx"
+TO_EXTENSION=".pdf"
+for File in "$SRC"/*"$FROM_EXTENSION"
+do
+    if [ -f "$File" ]
+        then
+            Filename=$(basename "$File")
+            name="${Filename%.*}"
+            new_filename="${name}$TO_EXTENSION"
+            mv "$File" "$SRC/$new_filename"
+        echo "$new_filename"
+    fi
+done
